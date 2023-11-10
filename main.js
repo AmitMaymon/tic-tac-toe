cbb7371b4b89a2dacedda156bc6c972f4119b293
Wolfy7725 <amit.maymon00@gmail.com>
2023-11-10 12:20:17 +0200

    Added row win condition check

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ usedTiles = []
 function clickBoard(num) {
     tile = document.getElementById(`td${num}`)
     rowId = tile.parentElement.id //gets The id of the tr element
-    rowId=rowId[rowId.length -1]
+    rowId = rowId[rowId.length - 1]
 
     console.log(`Row: ${rowId} Num: ${num}`) //DEBUG
 
@@ -29,8 +29,8 @@ function clickBoard(num) {
         tile.appendChild(x)
         turn++
         // TODO - insert win Condition
-        winCond('x',rowId,num)
-        trackMoves('x',num)
+        winCond('x', rowId, num)
+        trackMoves('x', num)
 
 
     } else {
@@ -42,32 +42,79 @@ function clickBoard(num) {
         tile.appendChild(o)
         turn++
         // TODO - insert win Condition
-        winCond('o',rowId,num)
-        trackMoves('o',num)
+        winCond('o', rowId, num)
+        trackMoves('o', num)
 
     }
 }
 grid = 3
-winTracker = [[[],[],[]],[[],[],[]],[[],[],[]]]
-function winCond(player,rowId,num){
-    for(let i = 0;i<grid;i++){
-        winTracker[i][rowId][num-(grid*rowId)] = player
+winTracker = []
+
+function winTrackerFill() {
+
+    for (let i = 0; i < 3; i++) {
+        winTracker.push([])
+        for (let j = 0; j < grid; j++) {
+            winTracker[i].push([])
+            for (let t = 0; t < grid; t++) {
+                winTracker[i][j][t] = '~'
+
+
+            }
+
+
+        }
+
+
+    }
+
+
+    console.log(winTracker)
+}
+winTrackerFill()
+
+
+function winCond(player, rowId, num) {
+    for (let i = 0; i < grid; i++) {
+        winTracker[i][rowId][num - (grid * rowId)] = player
     }
 
     console.log(winTracker)
+    // winCondRow()
+    winCondCol()
+
+
+}
+
+function winCondRow() {
+
+    for (let i = 0; i < grid; i++) {
+        let xtrack = 0
+        let otrack = 0
+        for (let j = 0; j < grid; j++) {
+
+
+            if (winTracker[0][i][j] == 'x') {
+
+                xtrack++
+            }
+            if (winTracker[0][i][j] == 'o') {
+                otrack++
+            }
+        }
+        if (xtrack == grid || otrack == 3) {
+            xtrack == grid ? console.log('x wins') : console.log('o wins')
+            break
+        }
+    }
+}
+function winCondCol() {
 
 
 
 
 }
-
-function winCondRow(){
-    
-}
-function winCondCol(){
-
-}
-function winCondDia(){
+function winCondDia() {
 
 }
 
@@ -90,19 +137,19 @@ function trackMoves(player, tile) {
 
 }
 
-function reDoMove(){
+function reDoMove() {
     lastImg = moveOrder.pop()
     img = document.getElementById(`tile${lastImg['tile']}`)
     img.remove()
 
     usedTiles.pop()
     index--
-    turn = turn -1
+    turn = turn - 1
 
 
     // console.log('UTiles: ',usedTiles,' Turn: ',turn,' Move Order: ',moveOrder) //DEBUG
 
-    reDoRemoveData(lastImg['player'],lastImg['tile'])
+    reDoRemoveData(lastImg['player'], lastImg['tile'])
 
 
 }
